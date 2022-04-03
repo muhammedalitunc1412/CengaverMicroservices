89ac97f762aa4a2b0c3089ec68a177c16406ef59
muhammed1412 <muhammed@retrace.ai>
2022-04-04 00:55:41 +0300

    Add Course Service

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICourseService.cs
@@ -0,0 +1,22 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    interface ICourseService
+    {
+        Task<Response<List<CourseDto>>> GetAllAsync();
+
+        Task<Response<CourseDto>> GetByIdAsync(string id);
+
+        Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string userId);
+
+        Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto);
+
+        Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto);
+
+        Task<Response<NoContent>> DeleteAsync(string id);
+    }
+}
