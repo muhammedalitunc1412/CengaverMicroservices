cc95166d7e006b13e321da3c3e136de0f3163a22
muhammed1412 <muhammed@retrace.ai>
2022-04-06 23:42:50 +0300

    Update

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/RedisService.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class RedisService
+    {
+        private readonly string _host;
+        private readonly int _port;
+        private ConnectionMultiplexer _ConnectionMultiplaxer;
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+        public void Connect() => _ConnectionMultiplaxer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+        public IDatabase GetDb(int db = 1) => _ConnectionMultiplaxer.GetDatabase(db);
+    }
+}
